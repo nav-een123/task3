1bf63d1808f3407e8dde973aee749d75d981d9a3
nav-een123 <naveen.500apps@gmail.com>
2025-02-12 14:48:02 +0530

    new commit

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
-from api.routers import route,task
+from api.routers import category
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-app.include_router(route.router)
-app.include_router(task.router)
+app.include_router(category.router)
+
+app.add_middleware(
+
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+
+)
+
+
---
+++ api/models/genericmodel.py
@@ -0,0 +1,28 @@
+from typing import Union
+from pydantic import BaseModel, Field, validator
+from datetime import datetime,time
+
+
+# user
+class createItems(BaseModel):
+    name: str
+    email:str
+    phone_number: str
+
+class updateItems(BaseModel):
+    name: str
+    email:str
+    phone_number: str
+   
+# category
+class createScheduleModel(BaseModel):
+    date: str
+    from_time:str
+    to_time:str
+    select_category: str = Field(..., description="The selected category")
+    
+class updateScheduleModel(BaseModel):
+    date: str
+    from_time:str
+    to_time:str
+    select_category: str = Field(..., description="The selected category")
